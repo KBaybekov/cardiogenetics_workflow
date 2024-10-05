2ec247b94a346d255dce05ef29089763ebf6304d
KBaybekov <91565993+KBaybekov@users.noreply.github.com>
2024-10-05 15:20:14 +0300

    1520

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-# Импортируем основные классы и функции, чтобы сделать их доступными при импорте модуля `src`.
-
-from .pipeline_manager import PipelineManager  # Основной класс для управления пайплайном
-from .module_runner import ModuleRunner          # Класс для выполнения стадий пайплайна
-from .command_executor import CommandExecutor  # Класс для выполнения команд
-from .cli import parse_cli_args                # Функция для парсинга CLI-аргументов
-from .utils import load_config, load_commands  # Вспомогательные функции для загрузки конфигураций
