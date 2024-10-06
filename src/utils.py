3db7fcfca7db44144e95e4bff14602ec51ed6e0e
KBaybekov <91565993+KBaybekov@users.noreply.github.com>
2024-10-07 01:01:09 +0300

    101

--- src/utils.py
+++ src/utils.py
@@ -121,9 +121,9 @@ def get_paths(folders: dict, input_dir: str, output_dir: str) -> dict:
     # Формируем словарь директорий с полными путями
     folders_with_paths = {
         # Проходим по всем директориям в 'input_dir' и добавляем базовый путь 'input_dir'
-        **{key: os.path.join(input_dir, f'{value}/') for key, value in folders.get('input', {}).items()},
+        **{key: os.path.join(input_dir, f'{value}/') for key, value in (folders.get('input') or {}).items()},
         # Проходим по всем директориям в 'output_dir' и добавляем базовый путь 'output_dir'
-        **{key: os.path.join(output_dir, f'{value}/') for key, value in folders.get('output', {}).items()}
+        **{key: os.path.join(output_dir, f'{value}/') for key, value in (folders.get('output') or {}).items()}
     }
     print(folders_with_paths)
     return folders_with_paths
