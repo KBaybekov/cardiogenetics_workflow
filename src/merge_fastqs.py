9d432568840c465caff2fd574c1e309a5e41aa4c
KBaybekov <91565993+KBaybekov@users.noreply.github.com>
2024-12-02 22:28:43 +0300

    2228

--- src/merge_fastqs.py
+++ src/merge_fastqs.py
@@ -12,7 +12,13 @@ out_dir = sys.argv[4]
 
 fqs = get_samples_in_dir(dir=fastq_dir, extensions=(fq_extension))
 ids = list(set(['_'.join(os.path.basename(fq).split('_')[:num_name_elements]) for fq in fqs]))
+groups = []
 for id in ids:
-    groups = {}
+    subset = [f for f in fqs if id in f]
+    if len(subset)> 2:
+        groups.append(id)
+    else:
+        os.system(f'mv {fastq_dir}{id}* {out_dir}{id}')
+for id in groups:
     for direction,substitute_direction in {'_R1_001':'_1', '_R2_001':'_2'}.items():
         os.system(f'cat {fastq_dir}{id}*{direction}{fq_extension} > {out_dir}{id}{substitute_direction}{fq_extension}')
