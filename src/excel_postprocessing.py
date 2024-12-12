58a5621521cad21b57657257035d4c853b949f3d
KBaybekov <91565993+KBaybekov@users.noreply.github.com>
2024-12-12 15:07:14 +0300

    1507

--- src/excel_postprocessing.py
+++ src/excel_postprocessing.py
@@ -159,7 +159,7 @@ def reform_data(data_df:pd.DataFrame, var_threshold:float, output_file:str, gene
             data_df.insert(alt_base_idx + 1, col, data_df.pop(col))
     # AF ставим всё же левее всех остальных
     data_df.insert(alt_base_idx + 1, 'Extra VCF INFO Annotations AF', data_df.pop('Extra VCF INFO Annotations AF'))
-    clinical_data_df = data_df[data_df['gnomAD Global AF'] <= var_threshold].reset_index(drop=True)
+    clinical_data_df = data_df[(data_df['gnomAD Global AF'] <= var_threshold) | (data_df['gnomAD Global AF'].isna())].reset_index(drop=True)
     if gene_panel:
         gene_panel_df = clinical_data_df[clinical_data_df['Gene'].isin(gene_panel)].reset_index(drop=True)
         return {output_file:data_df,
